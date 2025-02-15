da7cab30edbf966a238ea05c9f5a031da74ebf73
Natalia Marin <88326718+NataliaMarin490@users.noreply.github.com>
2025-02-14 19:48:33 -0600

    feat(sportify): add basic structure

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <main>
+        <h2>Contenido Principal</h2>
+        <p>Aquí va la información básica de del sitio web.</p>
+      </main>
+      <footer>
+        <p>2025 Sportify. Digital House.</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
