90dcdae7b767f9f4b07ed7bf4908e185d970def5
Natalia Marin <88326718+NataliaMarin490@users.noreply.github.com>
2025-02-15 09:28:56 -0600

    feat(sportify-frontend): sticky header, responsive, header navigation  add css variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import "./App.css";
-import Header from "./components/Header";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./Layouts/Layout ";
-import Home from "./Components/Home";
+import Home from "./Routes/Home";
+import Admin from "./Routes/Admin";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
       </Route>
     </Routes>
   );
---
+++ src/Routes/Home.jsx
@@ -0,0 +1,12 @@
+import '../Styles/home.css';
+
+const Home = () => {
+  return (
+    <main>
+      <h2>Contenido Principal</h2>
+      <p>Aquí va la información básica de del sitio web.</p>
+    </main>
+  );
+};
+
+export default Home;
