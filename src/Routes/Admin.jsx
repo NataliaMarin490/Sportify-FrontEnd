34e0898ee2250750d21c30c080bc3a47449608fa
lani23 <linalmg23@gmail.com>
2025-02-20 17:22:54 -0500

    feat: cambio para pantalla crear producto

--- src/Routes/Admin.jsx
+++ src/Routes/Admin.jsx
@@ -1,11 +1,13 @@
-import { useEffect } from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "../Styles/admin.css";
-import { useContextGlobal } from '../Context/global.context';
-import { useNavigate } from 'react-router-dom';
+import { useContextGlobal } from "../Context/global.context";
+import { useNavigate } from "react-router-dom";
 
 const Admin = () => {
-  const {state, dispatch} = useContextGlobal();
+  const { state, dispatch } = useContextGlobal();
   const navigate = useNavigate();
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     if (window) {
@@ -29,6 +31,20 @@ const Admin = () => {
   return (
     <div className="admin-view">
       <h1>Admin Panel</h1>
+      <div className="top-bar">
+        <input
+          type="text"
+          placeholder="Buscar..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button
+          onClick={() => navigate("/create-court")}
+          className="create-btn"
+        >
+          Crear
+        </button>
+      </div>
       <table>
         <thead>
           <tr>
@@ -53,6 +69,6 @@ const Admin = () => {
       {/* <button onClick={handleAdd}>Agregar Nueva Receta</button> */}
     </div>
   );
-}
+};
 
-export default Admin
+export default Admin;
