4fb23868e8970dab1e56bbf181294a400e0ed892
Natalia Marin <88326718+NataliaMarin490@users.noreply.github.com>
2025-02-19 23:56:04 -0600

    feat: add slider and fix cards display

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -3,17 +3,16 @@ import "../Styles/home.css";
 import Cards from "../components/Cards";
 import Recommendations from "../components/Recommendations";
 
-
 const Home = () => {
   const { state } = useContextGlobal();
 
   return (
     <>
-      <div className="contenedor-padre">
+      <div className="home-container">
         <div className="searcher-container">
           <div className="main-text-container">
             <h1>CANCHAS DEPORTIVAS</h1>
-            <p className="Texto">
+            <p className="text">
               Ofrecemos una amplia selección de canchas en todo el país, con
               precios <br />
               accesibles y condiciones óptimas para que disfrutes al máximo tu
@@ -28,45 +27,58 @@ const Home = () => {
             <button>Buscar</button>
           </div>
         </div>
-        <div className="ContenedorInfo">
-          <div className="Contenedor1">
-            <h4 className="Titulo">NUESTRAS CANCHAS MÁS RESERVADAS</h4>
-            <p className="Texto">
+        <div className="extra-info-container">
+          <div className="box-1">
+            <h4 className="box-title">NUESTRAS CANCHAS MÁS RESERVADAS</h4>
+            <p className="text">
               Conoce aquí las canchas más populares dentro de nuestros usuarios.
             </p>
+            <img
+              alt="arrow"
+              src="../../public/right-arrow.png"
+              className="right-arrow"
+            />
           </div>
-          <div className="Contenedor2">
-            <h4 className="Titulo">NUESTRAS CANCHAS MÁS RESERVADAS</h4>
-            <p className="Texto">
+          <div className="box-2">
+            <h4 className="box-title">NUESTRAS CANCHAS MÁS RESERVADAS</h4>
+            <p className="text">
               Conoce aquí las canchas más populares dentro de nuestros usuarios.
             </p>
+            <img
+              alt="arrow"
+              src="../../public/right-arrow.png"
+              className="right-arrow"
+            />
           </div>
-          <div className="Contenedor1">
-            <h4 className="Titulo">NUESTRAS CANCHAS MÁS RESERVADAS</h4>
-            <p className="Texto">
+          <div className="box-1">
+            <h4 className="box-title">NUESTRAS CANCHAS MÁS RESERVADAS</h4>
+            <p className="text">
               Conoce aquí las canchas más populares dentro de nuestros usuarios.
             </p>
+            <img
+              alt="arrow"
+              src="../../public/right-arrow.png"
+              className="right-arrow"
+            />
           </div>
         </div>
       </div>
       <main>
-        <div className="ContenedorPpal">
-          <div className="ContenedorCards">
+        <div className="main-content">
+          <div className="home-cards-container">
             {state.courts.map((court) => (
               <Cards key={court.id} court={court} />
             ))}
           </div>
         </div>
-        <div>
-          <h4 className="TituloReco">NUESTRAS CANCHAS MÁS RESERVADAS</h4>
-          <p className="TextoReco">
+        <div className="home-recommendations-container">
+          <h4 className="recommendations-title">
+            NUESTRAS CANCHAS MÁS RESERVADAS
+          </h4>
+          <p className="recommendations-text">
             Conoce aquí las canchas más populares dentro de nuestros usuarios.
           </p>
-          <div className="Reco">
-            {state.courts.map((court) => (
-              <Recommendations key={court.id} court={court} />
-            ))}
-          </div>
+          <Recommendations courts={state.courts} />
         </div>
       </main>
     </>
